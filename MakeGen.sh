e3a0f57c72253f097619428ba7e41e15c09eaa73
Danny <djpeck@iupui.edu>
2015-12-10 16:31:02 -0500

    close to finishing multiple args

--- MakeGen.sh
+++ MakeGen.sh
@@ -4,42 +4,65 @@
 # redirects the annoying dos2unix output into oblivion
 dos2unix $1/* 2> /dev/null
 
-# allows the command to work from any directory from within the local user
-dir=$(echo $1 | sed 's/.*\///g')
-
-# compiles the makeGen cpp program might be to specific at the moment
-g++ /usr/local/bin/MakeGen/makeGen.cpp -o makeG
-
 # if there is not a directory arugment passed in
 # then print error, clean up and exit
-if [ ! -d $(pwd)/$1 ] || [ ! $(pwd)/$1 ] ; then
-    echo "Did not specify directory argument: ./makeGen [Directory Name]"
-    rm ./makeG
-    exit 0
+
+sanitize() {
+    if [ ! $1 ] ; then
+	echo "Did not specify directory arguement: MakeGen [Directory Name]"
+	return 1;
+    elif [ ! -d $(pwd)/$1 ] ; then
+	echo "The given argument is not a valid directory"
+	return 1;
+    else
+	return 0;
+    fi
+}
+
+arg=$2
+
+if [ $1 = "-j" ] ; then
+    if sanitize $2 ; then
+	echo "Java"	
+    fi
 # if they did pass us a directory then list all of the files and find the .cpp's 
-else
-    cppFiles=$(ls -a $1 | grep .cpp)
+elif [ $1 = "-cpp" ] ; then
+    if sanitize $2 ; then
+	echo "Cpp"
+    fi
+elif sanitize $1 ; then
+    arg=$1
+
+    # compiles the makeGen cpp program might be to specific at the moment
+    g++ /usr/local/bin/MakeGen/makeGen.cpp -o makeG
+
+    cppFiles=$(ls -a $arg | grep .cpp)
+
+    # if there actually are cppFiles
     if [ ! -z "$cppFiles" ] ; then
 	# this command searches all of the .cpp files for user created include statements (#include "Book.h")
 	# and formats it into an exceptable format for passing into the makeGen c++ program for further parsing and manipulation
-	includes=$(grep -i "#include" $1/*.cpp | sed 's/.*>//g' | sed 's/#include//g' | sed 's/.*\///g' | sed 's/\"//g' | sed 's/: /:/g')
-
+	includes=$(grep -i "#include" $arg/*.cpp | sed 's/.*>//g' | sed 's/#include//g' | sed 's/.*\///g' | sed 's/\"//g' | sed 's/: /:/g')
+	
+	# allows the command to work from any directory from within the local user
+	dir=$(basename $arg)
+	
 	# runs the makeGen.cpp program with the cppFiles, include statements and directory argument
 	./makeG $cppFiles $includes $dir
-
-	# clean up the cpp program
-	rm ./makeG
+	
+	# clean up the current directory
+	rm makeG
 
 	# move the makefile into the given directory
-	mv ./makefile $1/
+	mv ./makefile $arg/
 
 	# run the make command and back out
-	cd $1
+	cd $arg
 	make
 	cd $(pwd)
     else
+	# report error and clean up directory
 	echo "No .cpp files found in directory"
-	rm ./makeG
-	exit 0
+	rm makeG
     fi
 fi
